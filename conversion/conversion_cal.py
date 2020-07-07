f56d965b97575acc84485bf9495b6caa37a89dca
Carlos Ochoa <cf_ochoa@hotmail.com>
2020-07-06 22:24:44 -0700

    Added kg

--- conversion/conversion_cal.py
+++ conversion/conversion_cal.py
@@ -1,11 +1,17 @@
 def sel():
     lbs = "lbs"
-    unit = input("Please type lbs for conversion: ")
+    kgs = "kgs"
+    unit = input("Please type lbs or kgs for conversion: ")
    
     if unit == lbs:
          transfer = float(input("What is the weight in lbs? "))
          kg = 0.453592 * transfer
-         print("The weight is  " + str(kg) + " Kilograms")
+         print("The weight is " + str(kg) + " Kilograms")
+    
+    elif unit == kgs:
+        transfer = float(input("What is the weight in kgs? "))
+        lbs = 2.20462 * transfer
+        print ("The weight is " + str(lbs) + " Pounds.")
         
     else: 
         print("Incorrect unit of conversion")
