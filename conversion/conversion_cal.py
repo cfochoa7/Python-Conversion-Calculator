f8df9462a1f0e4ebd6240ab17656f60133c5cd4b
Carlos Ochoa <cf_ochoa@hotmail.com>
2020-07-07 00:05:32 -0700

    added cm

--- conversion/conversion_cal.py
+++ conversion/conversion_cal.py
@@ -2,14 +2,15 @@ def sel():
     lbs = "lbs"
     kgs = "kgs"
     inches = "inches"
-    unit = input("Please type lbs/kgs or inches for conversion: ")
+    cm = "cm"
+    unit = input("Please type lbs, kgs, inches, or cm for conversion: ")
    
     if unit == lbs:
          transfer = float(input("What is the weight in lbs? "))
          kg = 0.453592 * transfer
          
          if kg == 0:
-             kg = 0.453592
+            kg = 0.453592
          else:
              pass
          
@@ -23,7 +24,14 @@ def sel():
     elif unit == inches:
         transfer = float(input("What is the length in inches? "))
         cm = 2.54 * transfer
-        print("The length is " + str(cm) + " Centimeters")
+        print("The length is " + str(cm) + "Centimeters")
+    
+    elif unit == cm:
+        transfer = float(input("What is the length in cm? "))
+        inch = 0.393701 * transfer
+        print("The length is " + str(inch) + " Inches")
+            
+       
         
     else: 
         print("Incorrect unit of conversion")
